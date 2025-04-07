c9781fb8c06ddc8b545b0929cee22f37a3cb7961
ford burgess <fburg6824@gmail.com>
2025-04-07 18:08:25 +0100

    some work for adam reeds page

--- src/features/articles/adamReedView.tsx
+++ src/features/articles/adamReedView.tsx
@@ -1,4 +1,6 @@
 import './adamReed.css'
+import Portrai1 from '../../assets/adam-reed.webp'
+import Portrai2 from '../../assets/adam-reed-2.jpeg'
 import { motion } from 'framer-motion'
 
 const AdamReedView  = () => {
@@ -29,9 +31,25 @@ const AdamReedView  = () => {
         >
           Adam shares his journey fom a small village salon in Somerset to doing Madonna’s hair. Delving into his love for hair, struggles and neurodiversity he encourages embracing individuality and trusting your decisions.
         </motion.p>
+        <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, ease: "easeOut", delay: 0.2 }}
+          style={{ display: 'flex', justifyContent: 'space-between', maxHeight: '350px', marginBottom: '50px' }}
+        >
+          <img src={Portrai1} className='portrait-1'/>
+          <img src={Portrai2} className='portrait-2'/>
+        </motion.div>
       </div>
-      <div>
-
+      <div style={{ textAlign: 'left' }}>
+        <motion.h2
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, ease: "easeOut", delay: 0.2 }}
+          className='clickable-subtitle'
+        >
+          When you were 21, who did you look up to in the beauty industry?
+        </motion.h2>
       </div>
     </div>
   )
