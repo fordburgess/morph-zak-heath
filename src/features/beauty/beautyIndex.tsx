ee7ccc2038b715459157457ca2b5cd6440431175
ford burgess <fburg6824@gmail.com>
2025-04-17 18:26:31 +0100

    scroll in effect sort of works

--- src/features/beauty/beautyIndex.tsx
+++ src/features/beauty/beautyIndex.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import FieldWide from '../../assets/field-wide.png';
 import FieldAbove from '../../assets/field-above.png';
 import FieldExpanded from '../../assets/field-expanded.jpg';
+import FieldExpandedTest from '../../assets/field-test.jpeg';
 import './styles/beautyIndex.css';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -17,47 +18,7 @@ const BeautyIndexView = () => {
   });
 
   useEffect(() => {
-//     gsap.registerPlugin(ScrollTrigger);
-//
-//     gsap.to('.expanded-image', {
-//       yPercent: 0, // move image up as you scroll (parallax effect)
-//       ease: 'none',
-//       scrollTrigger: {
-//         trigger: '.pinned-scroll-container',
-//         start: 'top top',
-//         end: 'top center',
-//         scrub: true, // link animation to scroll
-//       },
-//     });
 
-    // ScrollTrigger.create({
-    //   trigger: '.pinned-scroll-container',
-    //   start: 'center top',
-    //   end: 'top top',
-    //   scrub: true,
-    //   onUpdate: self => {
-    //     // Freeze position of the image by setting transform to fixed value
-    //     if (self.progress >= 0 && self.progress <= 1) {
-    //       gsap.set('.expanded-image', { yPercent: -20 });
-    //     }
-    //   }
-    // });
-
-    // gsap.fromTo(
-    //   '.scroll-in-content',
-    //   { opacity: 0, y: 100 }, // Starts off-screen and invisible
-    //   {
-    //     opacity: 1,
-    //     y: 0,
-    //     delay: 1, // Add a delay here (1 second) to wait before it starts scrolling
-    //     scrollTrigger: {
-    //       trigger: '.scroll-in-content',
-    //       start: 'top 60%', // When top of .scroll-in-content hits 80% of the viewport
-    //       end: 'top 40%', // When it’s around 60% of the viewport, it stops
-    //       scrub: true, // Smooth scrub between start and end
-    //     },
-    //   }
-    // );
   }, [])
 
 
@@ -68,9 +29,9 @@ const BeautyIndexView = () => {
         <div className='expanded-image-container'>
           <img src={FieldExpanded} className='expanded-image' />
         </div>
-        <div className='scroll-in-content'>
-          <p>Scroll In Content</p>
-        </div>
+      </div>
+      <div className='scroll-in-content'>
+        <p>Scroll In Content</p>
       </div>
     </div>
   )
--- src/features/podcasts/podcastIndex.jsx
+++ src/features/podcasts/podcastIndex.jsx
@@ -2,24 +2,33 @@ import { useEffect } from "react";
 
 const PodcastIndexView = () => {
 
-  const logScrollPosition = () => {
-    console.log('Scroll position:', window.scrollY);
-  };
-
-  useEffect(() => {
-    function addScroll() {
-      window.addEventListener('scroll', logScrollPosition);
-    }
-    addScroll()
-    return () => {
-      window.removeEventListener('scroll', logScrollPosition);
-    };
-  }, []);
+//   const logScrollPosition = () => {
+//     console.log('Scroll position:', window.scrollY);
+//   };
+//
+//   useEffect(() => {
+//     function addScroll() {
+//       window.addEventListener('scroll', logScrollPosition);
+//     }
+//     addScroll()
+//     return () => {
+//       window.removeEventListener('scroll', logScrollPosition);
+//     };
+//   }, []);
 
 
   return (
     <div style={{ height: '2000px', backgroundColor: 'gray' }}>
-
+      <div style={{ height: '100px', width: '100vw', backgroundColor: 'yellow' }}></div>
+      <div style={{
+        zIndex: 999,
+        height: '100px',
+        width: '100px',
+        backgroundColor: 'red',
+        position: 'sticky',
+        top: '-50px',
+        display: 'block',
+      }}></div>
     </div>
   )
 }
