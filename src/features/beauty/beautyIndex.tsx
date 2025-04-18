c8a9d8000cc12cdd2d197fa6815a73c957fd10ee
ford burgess <fburg6824@gmail.com>
2025-04-18 17:36:23 +0100

    got rid of redundant expanded image container

--- src/features/beauty/beautyIndex.tsx
+++ src/features/beauty/beautyIndex.tsx
@@ -46,9 +46,7 @@ const BeautyIndexView = () => {
     <div className='beauty-content-container'>
       <img src={FieldAbove} className='overhead-image' />
       <div ref={containerRef} className='pinned-scroll-container'>
-        <div className='expanded-image-container'>
-          <img src={FieldExpandedTest} className='expanded-image' />
-        </div>
+        <img src={FieldExpandedTest} className='expanded-image' />
       </div>
       <div className='scroll-in-content'>
         <div className='main-content'>
