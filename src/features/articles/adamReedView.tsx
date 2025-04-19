923377db27b99ced1d3b21d53627ea620558bd04
ford burgess <fburg6824@gmail.com>
2025-04-19 16:15:59 +0100

    before testing old bubbles

--- src/features/articles/adamReedView.tsx
+++ src/features/articles/adamReedView.tsx
@@ -9,73 +9,73 @@ const questions = [
   {
     question: 'What name would you have chosen, if you could have been called anything else?',
     answer: "Kestrel, like the bird. When I was young, I loved the film Kes, and I’ve always admired the power behind that name. Funny enough, when we adopted Riley (my husband and I’s son), before realising you couldn’t change their names, we almost called him Kestrel because of that connection.",
-    y: 200,
+    y: 400,
   },
   {
     question: "When you were 21, who did you look up to in the beauty industry?",
     answer: "Julien d’Ys, the French hairdresser. He has always been my ultimate icon. And Estée Lauder, I was obsessed with her brand. My grandmother used Estée Lauder and the heritage of the brand and what they did back then captivated me because it was’t as consumer facing. I also admired François Nars. Meeting Julien d’Ys was surreal; I completely fan-girled and went quiet, which isn’t like me at all! I stood quite in awe of the fact I got to meet him, and as it was early on in my career, I just thought he was amazing!",
-    y: 300,
+    y: 650,
   },
   {
     question: "What was your first interaction with hair?",
     answer: "It started with my Nan. As a boy, I’d go with her to this tiny salon in a tiny village in Somerset called Jenny’s Cut & Shape. It was full of old ladies getting shampoos and cut and I felt at home, me in a salon I did. I used to cut my sister’s and best friends hair from a young age and I guess that is why I have always worked in a salon and not been just a session stylist because I love that sense of community. I was only four, but they had me cleaning rollers, sweeping the floor, and making coffee. That salon gave me a sense of belonging that school never did, because I felt like I wasn’t very good at school, but in a salon I thrived, and I loved it.",
-    y: 500,
+    y: 200,
   },
   {
     question: "Where did you go clubbing in London at 21?",
     answer: "I went to Heaven, Velvet Underground, and a bar in Soho called Ricky Ticks, a hub for fashion people. Moving to London at 22, I immersed myself in these places to network and meet people, so I went three to four times a week, I loved it and I felt like I was with my people. I even met Jean Paul Gaultier at Heaven! I was wearing a Jean Paul Gaultier kilt, and so was he. I walked right up to him. Networking then was all face-to-face, and I loved it, so I made sure I was in the right places to meet the right people and you’d meet the most amazing people in Heaven.",
-    y: 400,
+    y: 550,
   },
   {
     question: "How did you approach networking when you didn’t know anyone?",
     answer: "I’d go up to people, compliment their work, and start conversations. My strategy was also to meet PRs face-to-face who could get me into events and who looked after these big names. The party scene was much bigger back then because social media wasn’t around to let people know what was happening, it was more exclusive, so having the PR’s numbers was a better way. Once there, my motto was kill people with kindness, so I avoided being obnoxious and focused on being helpful and approachable because that is what stood out.",
-    y: 100,
+    y: 300,
   },
   {
     question: "Did you face any setbacks in the industry?",
     answer: "Yes. I now understand that some of my struggles were tied to neurodiversity. I’ve always been sensitive to negativity and could feel overwhelmed in certain situations. Another challenge was proving that I wasn’t just a salon hairdresser and that I could do creative work and could manage a team behind the scenes at Fashion Week, and I had to battle with this. I persevered and assisted Luigi Murenu, an Italian hairstylist early on in my career, which helped me break through those barriers by supporting everything I wanted to do. They were amazing.",
-    y: 450,
+    y: 650,
   },
   {
     question: "What was your very first show?",
     answer: "It was for Red or Dead in 1996. They were a revolutionary streetwear brand who had a shop in Covent Garden, and I was obsessed with their clothes. It felt like a full-circle moment that they were my first London Fashion Week show since I loved their clothes and this made me massively believe in manifestation and putting things into the universe . From there, I worked with other amazing designers, including Andrew Groves, who was McQueen’s boyfriend at the time and whose shows really pushed boundaries - for example, one of his shows was called Cociane Nights and down the runway was a line of “coke”. I then started doing Julian McDonald, Matthew Williamson and then both the commercial and high-end fashion. I was really lucky that my trajectory took me to do all of that.",
-    y: 650,
+    y: 200,
   },
   {
     question: "What’s been your biggest “pinch me” moment so far?",
     answer: "You know what, it was doing Madonna’s hair for her Me Against the Music video with Britney Spears. I’ve always been a huge Madonna fan, so stepping into that role was surreal. For £60 in 2002, I bought some black heels from a charity auction on the Old Icon Madonna fan club as I am the biggest Madonna fan. Luigi Murenu used to do her hair but he was in Paris to do the Victor and Rolf show, so he asked me to go instead to Silver Cup Studios in New York. That was insane. Whilst doing her hair, Like A Prayer was being played on the radio, it felt like a everything had come full circle.",
-    y: 200,
+    y: 400,
   },
   {
     question: "Did you attend beauty school?",
     answer: "Yes, I worked in a salon and attended beauty school once a week to get my City & Guilds qualification and the school was called SCAT, Somerset College of Arts. Then it was very in- depth, we even learned how to make shampoo and perm solutions from scratch. When I moved to London at the age of twenty-two, I started at Charles Worthington.",
-    y: 300,
+    y: 600,
   },
   {
     question: "What’s your favourite fragrance?",
     answer: "Bloody hell, outside of my own line, I would say Diptyque’s 34.",
-    y: 350
+    y: 200
   },
   {
     question: "Your go-to restaurant in London?",
     answer: "Som Saa in Spitalfields. It’s a Northern Thai restaurant that started as a pop-up. I love the community spirit of it but the food above anything is the best Thai food outside of Thailand.",
-    y: 600,
+    y: 400,
   },
   {
     question: "Is there a fashion item you regret buying?",
     answer: "I’ve always used clothes as a bit of armour. I bought an outfit from a Central Saint Martins student because I loved what she made; however, I just felt uncomfortable in it. It wasn’t the outfit, it was me in it. Once, I fell down the escalator at Holborn Station wearing platform Buffalo boots. That was definitely a regret! I’ve always loved wearing skirts, but one I bought was too poofy and it was just so wrong for me.",
-    y: 200,
+    y: 250,
   },
   {
     question: "What would you tell your 21-year-old self now?",
     answer: "To always be proud of your decisions, because long term, it’s those things that give me the drive to carry on.",
-    y: 650,
+    y: 550,
   },
-  {
-    question: "Do you think your 21-year-old you would be like, fucking hell this is crazy?",
-    answer: "Without any shadow of a doubt. I was bullied and literally my whole life there have been people saying things; however, I hate confrontation and it’s the neurodiversity of understanding it. People really took the piss, but actually, every time I’ve gone with my gut, they’re the things that I’m proud of and it’s paid off, I did it and I’m still doing it.",
-    y: 340,
-  }
+  // {
+  //   question: "Do you think your 21-year-old you would be like, fucking hell this is crazy?",
+  //   answer: "Without any shadow of a doubt. I was bullied and literally my whole life there have been people saying things; however, I hate confrontation and it’s the neurodiversity of understanding it. People really took the piss, but actually, every time I’ve gone with my gut, they’re the things that I’m proud of and it’s paid off, I did it and I’m still doing it.",
+  //   y: 100,
+  // }
 ]
 
 const images = [
@@ -195,7 +195,7 @@ const AdamReedView = () => {
               context.font = '500 Josefin Sans';
             }
           } else {
-            context.font = '300 18px Josefin Sans';
+            context.font = '300 15px Josefin Sans';
           }
 
           const maxTextWidth = body.circleRadius * 1.6; // scale with size
@@ -272,18 +272,18 @@ const AdamReedView = () => {
       // Base radius is 80, then add some extra size based on the height of the text
       const baseSize = 80;
       const extra = Math.min(heightRequired * 0.5, 150); // max growth
-      return baseSize + extra + 25;
+      return baseSize + extra;
     };
 
     const colors = ['rgba(248, 215, 218, 0.7)', 'rgba(221, 235, 225, 0.7)', 'rgba(253, 246, 237, 0.7)'];
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
 
-    questions.map((item, i) => console.log((i + 1) * 200))
+    // questions.map((item, i) => console.log((i + 1) * 200))
 
     const circles = questions.map((item, i) =>
       Bodies.circle(
-        (i + 1) * 300,
+        (i + 1) * 100,
         item.y,
         getTextBasedRadius(render.context, item.question, item.answer),
         {
@@ -296,6 +296,7 @@ const AdamReedView = () => {
             questionOpacity: 1,
             answerOpacity: 0,
             fullTextVisible: false,
+            yPos: item.y
           },
           render: {
             fillStyle: colors[Math.floor(Math.random() * colors.length)],
@@ -403,14 +404,15 @@ const AdamReedView = () => {
       Composite.allBodies(world).forEach((body) => {
         if (body.isStatic) return;
 
+        // console.log(body.plugin)
         const deltaX = center.x - body.position.x;
-        const deltaY = center.y - body.position.y;
+        const deltaY = (body.plugin.yPos) - body.position.y;
         const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
         const driftForce1 = -0.0005; // negative x-axis for leftward motion
         const driftForce2 = -0.0002;
         Matter.Body.applyForce(body, body.position, {
           x: Common.random(driftForce1, driftForce2),
-          y: 0,
+          y: (deltaY / distance) * 0.05,
         });
 
         // -- jitter --
@@ -512,7 +514,7 @@ const AdamReedView = () => {
         ref={sceneRef}
         style={{
           background: 'transparent',
-          width: "300vw",
+          width: "1000vw",
           height: "100vh",
           margin: "auto",
         }}
