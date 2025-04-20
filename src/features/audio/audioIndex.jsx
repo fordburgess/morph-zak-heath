08d6607d7db636af2e69f135e72867765e094cc4
ford burgess <fburg6824@gmail.com>
2025-04-20 13:39:03 +0100

    initial scroll transition works

--- src/features/audio/audioIndex.jsx
+++ src/features/audio/audioIndex.jsx
@@ -1,34 +1,11 @@
-import { useEffect } from "react";
+import WideImage from '../../assets/winter-far.webp'
+
 
 const AudioIndexView = () => {
 
-//   const logScrollPosition = () => {
-//     console.log('Scroll position:', window.scrollY);
-//   };
-//
-//   useEffect(() => {
-//     function addScroll() {
-//       window.addEventListener('scroll', logScrollPosition);
-//     }
-//     addScroll()
-//     return () => {
-//       window.removeEventListener('scroll', logScrollPosition);
-//     };
-//   }, []);
-
-
   return (
-    <div style={{ height: '2000px', backgroundColor: 'gray' }}>
-      <div style={{ height: '100px', width: '100vw', backgroundColor: 'yellow' }}></div>
-      <div style={{
-        zIndex: 999,
-        height: '100px',
-        width: '100px',
-        backgroundColor: 'red',
-        position: 'sticky',
-        top: '-50px',
-        display: 'block',
-      }}></div>
+    <div className='page-container'>
+      <img src={WideImage} className='initial-image'/>
     </div>
   )
 }
--- src/features/services/servicesView.tsx
+++ src/features/services/servicesView.tsx
@@ -1,9 +1,73 @@
-
+import { useState, useEffect } from 'react';
+import WideImage from '../../assets/desert-far.webp';
+import OverheadImage from '../../assets/desert-aerial.webp';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 const ServicesView = () => {
+  const [overheadView, setOverheadView] = useState(false);
+
+  const handleImageChange = () => {
+    console.log("hello world")
+    setOverheadView(prev => !prev);
+
+    const initialImageContainer = document.querySelector('.initial-image-container');
+    const initialImage = document.querySelector('.initial-image');
+    const svgOverlay = document.querySelector('.svg-overlay-test');
+
+    initialImageContainer.style.transition = 'opacity 1.2s ease-in-out'
+    initialImage.style.transition = 'transform 0.3s ease-in-out';
+    svgOverlay.style.transition = 'opacity 0.5s ease-in-out';
+    svgOverlay.style.display = 'inline-block';
+
+    initialImage.style.transform = 'scale(2)';
+    initialImageContainer.style.opacity = 0;
+
+    requestAnimationFrame(() => {
+      svgOverlay.style.opacity = 1;
+    });
+
+    setTimeout(() => {
+      initialImageContainer.style.display = 'none';
+    }, 1000);
+  }
+
+  useEffect(() => {
+    gsap.to('.initial-image-container', {
+      scale: 2,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: ".page-container",
+        start: "top top",
+        end: "bottom top",
+        scrub: true
+      }
+    })
+
+    ScrollTrigger.create({
+      trigger: ".page-container",
+      start: "center top", // Adjust as needed
+      onEnter: () => {
+        handleImageChange();
+      },
+      // onLeaveBack: () => {
+      //   console.log('going back')
+      // }
+    });
+  }, [])
+
+  useEffect(() => {
+    console.log(overheadView)
+  }, [overheadView])
+
   return (
-    <div>
-      Services Page
+    <div className='page-container'>
+      <div className='initial-image-container'>
+        <h1 className='page-title'>Services</h1>
+        <img src={WideImage} className='initial-image' />
+      </div>
+      <img src={OverheadImage} className='svg-overlay-test' />
     </div>
   )
 }
