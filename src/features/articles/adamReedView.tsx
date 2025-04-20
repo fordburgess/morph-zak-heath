d9a51d9f38ae3d534ed6e0c7b36c523c23db0ee9
ford burgess <fburg6824@gmail.com>
2025-04-20 20:02:21 +0100

    installed matter js types

--- src/features/articles/adamReedView.tsx
+++ src/features/articles/adamReedView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import Matter from "matter-js";
 import MatterWrap from "matter-wrap"; // Make sure this is installed
 import "./styles/adamReed.css"; // Optional CSS
